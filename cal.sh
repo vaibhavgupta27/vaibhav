d2470c602be970a5dfd382f9767b79245dcdd707
vaibhavgupta27 <vaibhav.gupta76@yahoo.com>
2019-07-18 11:29:16 +0530

    hey

---
+++ cal.sh
@@ -0,0 +1,21 @@
+
+
+if [ $c -eq 1 ]
+then
+	s=$(( $a + $b ))
+	echo $s
+elif [$c -eq 2]
+then
+	s=$(( $a + $b ))
+	echo $s
+
+elif [$c -eq 3]
+then
+	s=$(( $a + $b ))
+	echo $s
+
+elif [$c -eq 4]
+then
+	s=$(( $a + $b ))
+	echo $s
+fi
